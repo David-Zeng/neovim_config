4239779024cbd5be19c0331e644ac1932455b75a
David-Zeng <david.zengdj@gmail.com>
2024-03-03 13:07:54 +1100

    add space+direct to jump between windows

--- lua/davidz/core/keymaps.lua
+++ lua/davidz/core/keymaps.lua
@@ -28,6 +28,11 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+keymap.set("n", "<leader>j", "<C-W>j")
+keymap.set("n", "<leader>k", "<C-W>k")
+keymap.set("n", "<leader>h", "<C-W>h")
+keymap.set("n", "<leader>l", "<C-W>l")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
